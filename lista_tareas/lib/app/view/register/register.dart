4e9150ae901da3b922aaa96a64d8a376ac2a9ef3
Lorena2387 <lorenaramirez23@hotmail.com>
2023-10-02 12:40:01 -0500

    implementation of the ChangeNotifierProvider in the textfield

--- lista_tareas/lib/app/view/register/register.dart
+++ lista_tareas/lib/app/view/register/register.dart
@@ -1,22 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class StatePage extends StatelessWidget {
   const StatePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Padding(
-        padding: EdgeInsets.all(50),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            _EmailTextField(),
-            SizedBox(
-              height: 20,
-            ),
-            _SendButton(),
-          ],
+    return ChangeNotifierProvider(
+      create: (_) => EmailProvider(),
+      child: const Scaffold(
+        body: Padding(
+          padding: EdgeInsets.all(50),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              _EmailTextField(),
+              SizedBox(
+                height: 20,
+              ),
+              _SendButton(),
+              SizedBox(
+                height: 20,
+              ),
+              _EmailText(),
+            ],
+          ),
         ),
       ),
     );
@@ -29,11 +37,15 @@ class _EmailTextField extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return TextField(
-      onChanged: (value) {},
+      onChanged: (value) => context.read<EmailProvider>().email = value,
       decoration: const InputDecoration(
         filled: true,
         fillColor: Colors.white,
-        border: OutlineInputBorder(),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.all(
+            Radius.circular(16),
+          ),
+        ),
         hintText: 'Email',
       ),
     );
@@ -49,5 +61,26 @@ class _SendButton extends StatelessWidget {
       onPressed: () {},
       child: const Text('Enviar'),
     );
+  }
+}
+
+class _EmailText extends StatelessWidget {
+  const _EmailText({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final email = context.watch<EmailProvider>().email;
+    return Text('El email introducido es $email');
+  }
+}
+
+class EmailProvider extends ChangeNotifier {
+  String _email = '';
+
+  String get email => _email;
+
+  set email(String value) {
+    email = value;
+    notifyListeners();
   }
 }
